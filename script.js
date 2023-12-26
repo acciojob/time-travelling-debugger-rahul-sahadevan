3715fd3eb48b7b9f1d0e7e8afff574a95b938950
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-12-26 16:25:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+function callStack(){
+	console.log("one");
+
+	return ()=>{
+		console.log('two');
+		setTimeout(()=>{
+			console.log('three');
+		},1000)
+	}
+}
+
+const result = callStack();
+result()
